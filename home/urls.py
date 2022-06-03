56c032c2632f52080242bc29a1323577b11c40b6
aayusha-aigel <aayush.adhikari@aigeal.com>
2022-06-03 15:54:00 +0545

    details page completed

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ from .views import *
 
 urlpatterns = [
 	path('',HomeView.as_view(),name = 'home'),
+	path('subcat/<slug>',SubcategoryView.as_view(),name = 'subcat'),
+	path('detail/<slug>',DetailView.as_view(),name = 'detail'),
+
+
 ]
