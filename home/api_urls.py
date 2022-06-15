8011cfd246b853dac69cf485e3b100b793a03c41
aayusha-aigel <aayush.adhikari@aigeal.com>
2022-06-15 16:11:53 +0545

    All solved

--- home/api_urls.py
+++ home/api_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
-from .views import ItemViewSet
+from .views import ItemViewSet,ItemFilterViewSet,CRUDItemViewSet
 
 
 # Routers provide an easy way of automatically determining the URL conf.
@@ -11,4 +11,7 @@ router.register(r'items', ItemViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('item_filter/',ItemFilterViewSet.as_view(),name = 'item_filter'),
+    path('crud_item/<int:pk>',CRUDItemViewSet.as_view(),name = 'crud_item'),
+
 ]
